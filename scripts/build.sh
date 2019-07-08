7f0b3c3cdc626e40001680f884e71498868b0c45
ShimizuNaoki <sensuikan1973@gmail.com>
2019-07-09 00:41:52 +0900

    fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ HOST='https://flutter-ffi-slide.done-sensuikan1973.com'
 
 # See: https://github.com/marp-team/marp-cli#metadata
 OG_TITLE='FlutterにおけるFFI'
-OG_DESCRIPTION="「FFIにおけるFlutter」at Flutter Meetup Tokyo"
+OG_DESCRIPTION="「Flutter における FFI」at Flutter Meetup Tokyo"
 OG_IMAGE="${HOST}/assets/icon.jpg"
 
 # 出力先をクリア
@@ -19,7 +19,7 @@ cpx 'assets/*.ico' 'dist/'
 # NOTE: ローカルで作らないと文字化けしちゃうので、こうしてる。
 # https://github.com/marp-team/marp-cli/blob/a410975992c3ea82dfe26c67b6b955cb142755dd/src/config.ts#L211 とかを見る感じ、
 # lang 関連を設定すればいけそうな雰囲気あったが無理だった。残念。
-cps 'slide.pdf' 'dist/'
+cpx 'slide.pdf' 'dist/'
 
 marp --html $SRC --output $OUTPUT \
 --theme $THEME \
